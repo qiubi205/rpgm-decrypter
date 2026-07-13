d235f333f8fb7a61ce0ec82bdca41b396e9c3f6c
Build <build@local>
2026-07-13 13:23:01 +0800

    fix: disable minify for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,11 +16,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
         }
     }
 
